af4a97abdf9326ae3c77885bb3f005c9314f47c3
aflori <aurelien.flori@le-campus-numerique.fr>
2023-08-04 09:33:29 +0200

    added a Game class' variable in main

--- Main.java
+++ Main.java
@@ -5,21 +5,27 @@ public class Main {
 
     public static void main(String[] args) {
         Menu menu = new Menu();
-        Personnage playerCharacter;
-
+        Personnage playerCharacter = null;
         Menu.PossibleReturn menuAction;
+        BoardGame playerGame;
+
         do {
+
             menuAction = menu.showMenu();
 
             switch (menuAction){
+
                 case CREATE_CHARACTER -> playerCharacter = menu.createCharacter();
+                case START_GAME -> {
+                    playerGame = new BoardGame(playerCharacter);
+                }
             }
         }while(menuAction!= Menu.PossibleReturn.QUIT_GAME);
 
         System.out.println("Merci d'avoir joué");
     }
 
-    /*
+
     public static int randomInt(int vMin, int vMax)
     {
         if (vMin >= vMax) {
@@ -29,7 +35,5 @@ public class Main {
         Random r = new Random();
         return r.nextInt((vMax - vMin) + 1) + vMin;
     }
-
-     */
 }
 
