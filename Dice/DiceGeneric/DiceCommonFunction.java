f6fdfd8f977ca646a86aad893d1d0665367f60d9
aflori <aurelien.flori@le-campus-numerique.fr>
2023-08-09 11:20:03 +0200

    fix: 1 Faced Case Random

--- Dice/DiceGeneric/DiceCommonFunction.java
+++ Dice/DiceGeneric/DiceCommonFunction.java
@@ -9,7 +9,9 @@ class DiceCommonFunction {
     private static final Random r = new Random();
 
     public static int randomInt(int vMin, int vMax) {
-        if (vMin >= vMax) {
+        if(vMin == vMax) return vMax;
+
+        if (vMin > vMax) {
             throw new IllegalArgumentException("max must be greater than min");
         }
 
