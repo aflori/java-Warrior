5b13a924c4357210fd1b0b1f354901412acb3a81
aflori <aurelien.flori@le-campus-numerique.fr>
2023-08-11 15:16:23 +0200

    random case reorder

--- GameElement/BoardGame.java
+++ GameElement/BoardGame.java
@@ -5,8 +5,9 @@ import Enemy.Goblin;
 import Enemy.Wizard;
 import Exception.*;
 import GameElement.CaseElement.*;
+import ProgramRandom.MyPersonnalRandom;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BoardGame {
@@ -49,8 +50,97 @@ public class BoardGame {
         return caseContent.get(positionPlayer - 1);
     }
 
-    private void generateBoardWithRandomCase(){
-        Case[] baseElement = {new SpellCase("éclair", 2), new WeaponCase("massues", 3), new EnemyCase(new Goblin()), new SpellCase("éclair", 2), new WeaponCase("massues", 3), new EnemyCase(new Goblin()), new PotionCase("potion", 2), new SpellCase("éclair", 2), new EnemyCase(new Goblin()), new EnemyCase(new Wizard()), new WeaponCase("massues", 3), new EnemyCase(new Goblin()), new PotionCase("potion", 2), new EmptyCase(), new EnemyCase(new Goblin()), new EmptyCase(), new SpellCase("éclair", 2), new EnemyCase(new Goblin()), new WeaponCase("épée", 5), new EnemyCase(new Wizard()), new EnemyCase(new Goblin()), new WeaponCase("massues", 3), new SpellCase("éclair", 2), new EnemyCase(new Goblin()), new EnemyCase(new Wizard()), new WeaponCase("épée", 5), new EnemyCase(new Goblin()), new PotionCase("grande potion", 5), new EmptyCase(), new EnemyCase(new Goblin()), new PotionCase("potion", 2), new EnemyCase(new Wizard()), new PotionCase("potion", 2), new EmptyCase(), new EnemyCase(new Wizard()), new EnemyCase(new Wizard()), new EnemyCase(new Wizard()), new WeaponCase("massues", 3), new PotionCase("potion", 2), new EnemyCase(new Wizard()), new PotionCase("grande potion", 5), new WeaponCase("épée", 5), new PotionCase("potion", 2), new EnemyCase(new Wizard()), new EnemyCase(new Dragon()), new EmptyCase(), new EnemyCase(new Wizard()), new SpellCase("boules de feu", 7), new SpellCase("boules de feu", 7), new EmptyCase(), new EmptyCase(), new EnemyCase(new Dragon()), new WeaponCase("épée", 5), new EmptyCase(), new EmptyCase(), new EnemyCase(new Dragon()), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EnemyCase(new Dragon()), new EmptyCase(), new EmptyCase()};
-        this.caseContent =  Arrays.asList(baseElement);
+    private void generateBoardWithRandomCase() {
+        MyPersonnalRandom r = new MyPersonnalRandom();
+        Case[][] listElement = new Case[][]{{ // contain 64 Case sorted by its content
+                new EnemyCase(new Dragon()), new EnemyCase(new Dragon()), new EnemyCase(new Dragon()), new EnemyCase(new Dragon())
+        }, {
+                new EnemyCase(new Wizard()), new EnemyCase(new Wizard()), new EnemyCase(new Wizard()), new EnemyCase(new Wizard()), new EnemyCase(new Wizard()), new EnemyCase(new Wizard()), new EnemyCase(new Wizard()), new EnemyCase(new Wizard()), new EnemyCase(new Wizard()), new EnemyCase(new Wizard())
+        }, {
+                new EnemyCase(new Goblin()), new EnemyCase(new Goblin()), new EnemyCase(new Goblin()), new EnemyCase(new Goblin()), new EnemyCase(new Goblin()), new EnemyCase(new Goblin()), new EnemyCase(new Goblin()), new EnemyCase(new Goblin()), new EnemyCase(new Goblin()), new EnemyCase(new Goblin())
+        }, {
+                new WeaponCase("massue", 3), new WeaponCase("massue", 3), new WeaponCase("massue", 3), new WeaponCase("massue", 3), new WeaponCase("massue", 3)
+        }, {
+                new WeaponCase("épée", 5), new WeaponCase("épée", 5), new WeaponCase("épée", 5), new WeaponCase("épée", 5)
+        }, {
+                new SpellCase("éclair", 2), new SpellCase("éclair", 2), new SpellCase("éclair", 2), new SpellCase("éclair", 2), new SpellCase("éclair", 2)
+        }, {
+                new SpellCase("boule de feu", 7), new SpellCase("boules de feu", 7)
+        }, {
+                new PotionCase("potion", 2), new PotionCase("potion", 2), new PotionCase("potion", 2), new PotionCase("potion", 2), new PotionCase("potion", 2), new PotionCase("potion", 2)
+        }, {
+                new PotionCase("grande potion", 5), new PotionCase("grande potion", 5)
+        }, {
+                new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(), new EmptyCase(),
+        }};
+
+        this.caseContent = new ArrayList<>(64);
+        this.caseContent.add(listElement[9][0]);
+
+        listElement[9][0] = null;
+
+        //case 2-10
+        int[] probabilityAppearance = {0, 0, 3, 2, 0, 2, 0, 1, 0, 1};
+        moveElement(2, 10, probabilityAppearance, listElement, r);
+
+        //case 10-20
+        probabilityAppearance = new int[]{0, 1, 6, 5, 0, 5, 0, 1, 0, 1};
+        moveElement(10, 20, probabilityAppearance, listElement, r);
+
+        //case 20-30
+        probabilityAppearance = new int[]{0, 5, 10, 8, 3, 8, 0, 2, 0, 1};
+        moveElement(20, 30, probabilityAppearance, listElement, r);
+
+        //case 30-45
+        probabilityAppearance = new int[]{0, 10, 100, 100, 5, 100, 2, 4, 3, 1};
+        moveElement(30, 45, probabilityAppearance, listElement, r);
+
+        //case 45-55
+        probabilityAppearance = new int[]{1, 100, 100, 100, 10, 100, 100, 10, 10, 4};
+        moveElement(45, 55, probabilityAppearance, listElement, r);
+
+        //case 55-65
+        probabilityAppearance = new int[]{1, 100, 100, 100, 100, 100, 100, 100, 0, 1};
+        moveElement(55, 65, probabilityAppearance, listElement, r);
+
+    }
+
+    private void moveElement(int startIndex, int endIndex, int[] probabilityAppearance, Case[][] listElement, MyPersonnalRandom r) {
+        for (int i = startIndex; i < endIndex; i++) {
+            int randomValue = 0;
+            for (int j = 0; j < probabilityAppearance.length; j++) {
+                if (isNotEmpty(listElement[j])) {
+                    randomValue += probabilityAppearance[j];
+                }
+            }
+            randomValue = r.randomInt(1, randomValue);
+
+            for (int j = 0; j < probabilityAppearance.length; j++) {
+                if (isNotEmpty(listElement[j])) {
+                    if (randomValue <= probabilityAppearance[j]) {
+                        this.moveToList(listElement[j]);
+                        break;
+                    } else {
+                        randomValue -= probabilityAppearance[j];
+                    }
+                }
+            }
+        }
+    }
+
+    private void moveToList(Case[] array) {
+        int i = 0;
+        while (array[i] == null) {
+            i++;
+        }
+        this.caseContent.add(array[i]);
+        array[i] = null;
+    }
+
+    private static boolean isNotEmpty(Case[] list) {
+        for (Case element : list) {
+            if (element != null) return true;
+        }
+        return false;
     }
 }
