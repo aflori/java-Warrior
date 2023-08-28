9ab0c6d7817299c2ab3f6743b087dfb661305555
asmuro1 <f.aurelien@yahoo.fr>
2023-08-28 15:25:05 +0200

    finished constructor

--- jdbc_interaction/hero_src.java
+++ jdbc_interaction/hero_src.java
@@ -3,20 +3,18 @@ package jdbc_interaction;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class hero_src {
 
-    public hero_src() {
-        try {
-            String protocole = "jdbc:mysql", ip = "localhost", port = "3306", baseName = "java_warrior";
-            String userAccount, userPassword;
+    private Statement requestLaunch;
 
+    public hero_src() throws SQLException {
+        String protocole = "jdbc:mysql", ip = "localhost", port = "3306", baseName = "java_warrior";
+        String userAccount, userPassword;
 
-            Connection databaseSrc = DriverManager.getConnection(protocole + "://" + ip + ":" + port + "/" + baseName, userAccount, userPassword);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        Connection databaseSrc = DriverManager.getConnection(protocole + "://" + ip + ":" + port + "/" + baseName, userAccount, userPassword);
+        this.requestLaunch = databaseSrc.createStatement();
     }
 
 }
