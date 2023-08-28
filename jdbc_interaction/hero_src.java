73d2e6da81f6de74d7d62a04d22de0a234c6de16
asmuro1 <f.aurelien@yahoo.fr>
2023-08-28 15:52:39 +0200

    implemented method getHeroesList()

--- jdbc_interaction/hero_src.java
+++ jdbc_interaction/hero_src.java
@@ -1,9 +1,10 @@
 package jdbc_interaction;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import Character.*;
 
 public class hero_src {
 
@@ -17,4 +18,33 @@ public class hero_src {
         this.requestLaunch = databaseSrc.createStatement();
     }
 
+    public Set<Personnage> getHeroesSet() {
+        Set<Personnage> characterSet = new HashSet<Personnage>();
+
+        ResultSet personageList = null;
+        try {
+            personageList = this.requestLaunch.executeQuery("SELECT * FROM heroes;");
+
+            while (personageList.next()) {
+                Personnage extractedCharacter = null;
+                String type = personageList.getString("type");
+                if (type.equals("magician")) {
+                    extractedCharacter = new Magicien();
+                } else {
+                    extractedCharacter = new Guerrier();
+                }
+                extractedCharacter.setAttackPower(personageList.getInt("attackPoint"));
+                extractedCharacter.setName(personageList.getString("name"));
+                extractedCharacter.setLifePoint(personageList.getInt("lifePoint"));
+
+                //no weapons/philter at the start of the game for now
+
+                characterSet.add(extractedCharacter);
+            }
+        } catch (SQLException e) {
+            return characterSet;
+        }
+
+        return characterSet;
+    }
 }
